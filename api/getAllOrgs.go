948541e1a6f1e87d231f704f6d63725977d0195d
SomeYoGuyeee <145333892+SomeYoGuyeee@users.noreply.github.com>
2023-09-26 12:30:14 +0530

    Update getAllOrgs.go to add comments

--- api/getAllOrgs.go
+++ api/getAllOrgs.go
@@ -8,8 +8,16 @@ import (
 )
 
 func getAllOrgs(c *gin.Context) {
+	
+	// The following function creates a client instance for the database 
+	// GetAllParentOrgs fn is called on this object instance
+	//
+	// defined at: ../database/handlers/client.go
+	// returns:    Client object defined with function definition
 	client := handlers.New()
 	defer client.Close()
 
+	// The following function returns the fetched data. 
+	// GetAllParentOrgs is defined at ../database/handlers/parentOrg.go
 	c.IndentedJSON(http.StatusOK, client.GetAllParentOrgs())
 }
