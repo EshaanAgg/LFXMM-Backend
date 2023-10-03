859dc1de9608988b17aae570c106a2d729474a42
SilverDragonOfR <ptripathy013@gmail.com>
2023-10-03 19:19:48 +0530

    Modified ProjectThumbail for more fields

--- database/responses.go
+++ database/responses.go
@@ -1,10 +1,23 @@
 package database
 
+// type ProjectThumbail struct {
+// 	ID          string `json:"id"`
+// 	ProjectURL  string `json:"projectUrl"`
+// 	Name        string `json:"name"`
+// 	Description string `json:"description"`
+// 	ProgramYear int    `json:"programYear"`
+// 	ProgramTerm string `json:"programTerm"`
+// }
+
 type ProjectThumbail struct {
-	ID          string `json:"id"`
-	ProjectURL  string `json:"projectUrl"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	ProgramYear int    `json:"programYear"`
-	ProgramTerm string `json:"programTerm"`
+	ID          string   `json:"id"`
+	Name        string   `json:"name"`
+	Industry    []string `json:"industry"`
+	Description string   `json:"description"`
+	Skills      []string `json:"skills"`
+	Website     string   `json:"website"`
+	Repository  string   `json:"repository"`
+	ProgramYear int      `json:"programYear"`
+	ProgramTerm string   `json:"programTerm"`
+	ProjectURL  string   `json:"projectUrl"`
 }
